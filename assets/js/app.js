993331193b1eba79dc41ac36f481014ac0bce10d
darienjonathan <darien-winata@kayac.com>
2018-12-06 14:20:28 +0900

    refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,50 @@
+const heightConfig = {
+  baseHeight: 3,
+  elementInterval: 2
+}
 
+const { baseHeight, elementInterval } = heightConfig;
 
-$(document).ready(() => {
-  $(window).on("load", () => {
-    $(".loading").addClass("fadeOut");
-    setTimeout(() => $(".wrapper").addClass("fadeIn"), 500);
-    $(".content__video").get().forEach(vid => vid.volume = 0);
+const elementPosition = {
+  prompt: 0,
+  daisy: baseHeight,
+  video: baseHeight + elementInterval,
+  goodluck: baseHeight + 2*elementInterval,
+  pray: baseHeight + 3*elementInterval,
+  havefun: baseHeight + 4*elementInterval,
+  closing: baseHeight + 5*elementInterval,
+};
 
-    $(".prompt-button").on('click', promptClick);
-    $(window).on('scroll', _.throttle(scroll, 25));
-  });
-});
+const calc = {
+  // op = -(y - k)((y - k) - 2) dimana op = 1 (opacityMax) terjadi pada y = (k + k + 2)/2 = k + 1. Untuk mencapai opacityMax pada k, maka op = -(y - (k - 1))(y - (k - 1) - 2).
+  // agar op = 1 tercapai lebih cepat, maka f(y) dibagi dengan bilangan positif < 1. 
+  opacity: maxOpacityPos => -1*(window.pageYOffset/window.innerHeight - (maxOpacityPos - 1))*(window.pageYOffset/window.innerHeight - (maxOpacityPos - 1) - 2)/0.7,
+  // vol = (y - k)/k + 1 dimana vol = 1 (max) pada y = k. 
+  volume: maxVolumePos => {
+    let volume = ((window.pageYOffset/window.innerHeight - maxVolumePos)/maxVolumePos + 1);
+    volume = volume < 0 ? 0 : volume > 1 ? 1 : volume;
+    return volume;
+  }
+}
 
+const controlOpacity = () => {
+  $(".box--prompt").css('opacity', calc.opacity(elementPosition.prompt));
+  $(".box--daisy").css('opacity', calc.opacity(elementPosition.daisy));
+  $(".box--video").css('opacity', calc.opacity(elementPosition.video));
+  $(".box--goodluck").css('opacity', calc.opacity(elementPosition.goodluck));
+  $(".box--pray").css('opacity', calc.opacity(elementPosition.pray));
+  $(".box--havefun").css('opacity', calc.opacity(elementPosition.havefun));
+  $(".box--closing").css('opacity', calc.opacity(elementPosition.closing));
+}
+
+const controlGain = (audioSource, gainNode) => {
+  let audioVolume = calc.volume(elementPosition.video);
+  gainNode.gain.value = audioVolume;
+  audioSource.connect(gainNode);
+
+  console.log(gainNode);
+  console.log({ yPos, audioVolume });
+};
 
 const loadSound = () => {
   var context = 'AudioContext' in window
@@ -30,84 +64,32 @@ const loadSound = () => {
       gainNode.connect(context.destination);
       source.connect(gainNode);
       source.start(0);
-      $(window).on('scroll', _.throttle(() => controlGain(source, gainNode), 25));
+      $(window).on('scroll', _.throttle(() => {
+        controlGain(source, gainNode);
+        controlOpacity();
+      }, 25));
     }, function(err){
       console.log(`Error: ${err}`);
     });
   };
+  req.onerror = err => $(window).on('scroll', _.throttle(controlOpacity, 25));
   req.send();
 }
 
 const promptClick = () => {
-  loadSound();
   $(".wrapper").removeClass("wrapper--pre-click").addClass("wrapper--post-click");
-  $(".content__video").get().forEach(vid => {
-    let foo = vid.play();
-    foo
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
-  });
+  $(".content__video").get().forEach(vid => vid.play());
   $(".box--prompt-button").addClass("fadeOut");
   setTimeout(() => $(".box--prompt-scroll").addClass("fadeIn"), 500);
+
+  loadSound();
 };
 
-const scroll = () => {
-  const baseHeightMultiplier = 3;
-  const elementInterval = 2;
-  const yPos = window.pageYOffset;
-  const height = window.innerHeight;
-  const calcOpacity = maxOpacityPos => -1*(yPos - (maxOpacityPos - 1)*height)*(yPos - (maxOpacityPos - 1)*height - 2*height)/(height*height*0.7); //max opacity in maxOpacityPos
-  let elementOpacity = {
-    prompt: calcOpacity(0),
-    daisy: calcOpacity(baseHeightMultiplier),
-    video: calcOpacity(baseHeightMultiplier + elementInterval),
-    goodluck: calcOpacity(baseHeightMultiplier + 2*elementInterval),
-    pray: calcOpacity(baseHeightMultiplier + 3*elementInterval),
-    havefun: calcOpacity(baseHeightMultiplier + 4*elementInterval),
-    closing: calcOpacity(baseHeightMultiplier + 5*elementInterval),
-  };
+$(document).ready(() => {
+  $(window).on("load", () => {
+    $(".loading").addClass("fadeOut");
+    setTimeout(() => $(".wrapper").addClass("fadeIn"), 500);
 
-  $(".box--prompt").css('opacity', elementOpacity.prompt);
-  $(".box--daisy").css('opacity', elementOpacity.daisy);
-  $(".box--video").css('opacity', elementOpacity.video);
-  $(".box--goodluck").css('opacity', elementOpacity.goodluck);
-  $(".box--pray").css('opacity', elementOpacity.pray);
-  $(".box--havefun").css('opacity', elementOpacity.havefun);
-  $(".box--closing").css('opacity', elementOpacity.closing);
-
-  // const videoMaxVolume = baseHeightMultiplier + elementInterval + 1;
-  // let videoVolume = ((yPos - videoMaxVolume*height)/videoMaxVolume + height)/(2*height);
-  // videoVolume =
-  //   videoVolume < 0
-  //   ? 0
-  //   : videoVolume > 0.5
-  //   ? 0.5
-  //   : videoVolume;
-
-  // $(".content__video").get().forEach(vid => vid.volume = videoVolume);
-  // $(".content__video").get().forEach(vid => console.log(vid.volume));
-
-  // console.log({ yPos, videoVolume });
-}
-
-const controlGain = (source,gainNode) => {
-  const baseHeightMultiplier = 3;
-  const elementInterval = 2;
-  const yPos = window.pageYOffset;
-  const height = window.innerHeight;
-
-  const audioMaxVolume = baseHeightMultiplier + elementInterval + 1;
-  let audioVolume = ((yPos - audioMaxVolume*height)/audioMaxVolume + height)/(2*height);
-  audioVolume =
-    audioVolume < 0
-    ? 0
-    : audioVolume > 1
-    ? 1
-    : audioVolume;
-
-  gainNode.gain.value = audioVolume;
-  source.connect(gainNode);
-
-  console.log(gainNode);
-  console.log({ yPos, audioVolume });
-}
+    $(".prompt-button").on('click', promptClick);
+  });
+});
