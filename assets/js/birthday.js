fd0aef32d8e80dc949684216e7e9b081825d13af
darienjonathan <darien-winata@kayac.com>
2018-12-21 12:48:01 +0900

    test

---
+++ assets/js/birthday.js
@@ -0,0 +1,37 @@
+// const birthdayTime = 1545411600000;
+const birthdayTime = 1545364187781;
+const tempo = 74;
+const frame = 16;
+
+$(document).ready(() => {
+  $(window).on("load", () => {
+    $(".wrapper").addClass("fade-in");
+    const now = new Date().getTime();
+    if(now > birthdayTime) {
+      $(".loading").addClass("fade-out");
+      $(".prompt").addClass("fade-in");
+      $(".prompt-play").addClass("fade-in");
+      $(".prompt-button").on('click', () => {
+        $(".prompt").addClass("fade-out");
+        setTimeout(() => {
+          $(".audio")[0].play();
+          $(".lyrics").each((index, el) => {
+            setTimeout(() => {
+              $(el).siblings().removeClass("fade-in");
+              setTimeout(() => $(el).addClass("fade-in"), 1000);
+            }, 1000*((frame/tempo*60)*index - 1.75))
+          })
+          $(".audio").on("ended", () => {
+            console.log("audio ended");
+            $(".lyrics").removeClass("fade-in");
+            $(".greeting").addClass("fade-in");
+          })
+        }, 1000*(3/tempo*60))
+      })
+    } else {
+      $(".loading").addClass("fade-out");
+      $(".prompt").addClass("fade-in");
+      $(".prompt-wait").addClass("fade-in");
+    }
+  })
+});
